143f89370a56f85599b701997e529089ae29685c
Ahmed elattar <ahmeddelattarr@outlook.com>
2024-09-03 22:38:38 +0300

    making the models and a basic file structure for the project

---
+++ src/tickets/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Movie(models.Model):
+    movie = models.CharField(max_length=50)
+    hall = models.CharField(max_length=10)
+    reservation = models.DateField()
+
+class Guest(models.Model):
+    name = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=11)
+
+class Reservation(models.Model):
+    guest = models.ForeignKey(Guest, related_name='reservations', on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, related_name='movie_reservations', on_delete=models.CASCADE)
