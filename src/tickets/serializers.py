0bbc87c0ba2d9db51c507bd6d92bd19d148751a7
Ahmed elattar <ahmeddelattarr@outlook.com>
2024-09-03 23:14:13 +0300

    add serializers for Movie ,guest,reservation

---
+++ src/tickets/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Movie,Guest,Reservation
+
+class GuestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Guest
+        fields='__all__'
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Movie
+        fields='__all__'
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Reservation
+        fields=['pk',
+                'reservations',
+                'name',
+                'mobile']
